6a3e36416dcdc29ef8fc3e42968593a82a7ead06
sergioleite <sergio.leite.silva@gmail.com>
2025-03-24 05:25:38 -0300

    Retirei a pasta módulos

--- conecte-poc-terraform-iac/modules/key-vault-policy/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_key_vault_access_policy" "akv_policy" {
-  key_vault_id = var.kv_id
-  tenant_id    = var.tenant_id
-  object_id    = var.object_id
-
-  secret_permissions = [
-      "Get", "List"
-    ]
-}
